9abdddaec400a45221e6e0e3a1bb6784aa866efb
nicholasmullikin <nicholasmullikin@users.noreply.github.com>
2018-02-09 02:10:50 -0500

    Added Plotly.js grapher

--- project/static/javascript/data.js
+++ project/static/javascript/data.js
@@ -11,29 +11,52 @@ $(document).ready(function () {
                 max: csv_length,
                 min: 1,
                 slide: function(event, ui) {
-                    start_value = ui.value -10; // get the other side of values -- eventually want to make this modifiable too
-                    $.ajax({url: '/data-range/',data: "start="+start_value+"&end="+ui.value , success: function(result){
+                    difference = 10;
+                    end_value = ui.value;
+                    if( end_value - difference < 2){ // first line is headers
+                    end_value = difference + 2;
+                    }
+                    start_value = end_value - difference; // get the other side of values -- eventually want to make this modifiable too
+                    $.ajax({url: '/data-range/',data: "start="+start_value+"&end="+end_value , success: function(result){
                         current_data = result;
                         set_sidebar(current_data[0]);
+                        graph(current_data)
                     }});
                 }
             }).width(width - $(".sidenav").width() - 50);
          } );
     }});
+    function graph(data){
+        var altitude = [{
+            x: [],
+            y: [],
+            type: 'scatter'
+        }];
+        for (var i = 0; i < data.length; i++){
+            altitude[0].x[i]= reformat_date(data[i][0]);
+            altitude[0].y[i]= parseFloat(data[i][1]);
+        }
+        console.log(altitude);
+        Plotly.newPlot('altitude_graph', altitude);
+    }
+    function reformat_date(date){
+        date = date.split(":");
+        return (date[0]+'-'+date[1]+'-'+date[2]+' '+date[3]+':'+date[4]+":"+date[5])
+    }   // needs to be 2013-10-04 22:23:00
 
     function set_sidebar(data_array){
-    $("#altitude").text("Altitude: "+data_array[1])
-    $("#pressure").text("Pressure: "+data_array[2])
-    $("#temperature").text("Temperature: "+data_array[3])
-    $("#gyrox").text(data_array[4])
-    $("#gyroy").text(data_array[5])
-    $("#gyroz").text(data_array[6])
-    $("#magx").text(data_array[7])
-    $("#magy").text(data_array[8])
-    $("#magz").text(data_array[9])
-    $("#rhall").text(data_array[10])
+        $("#altitude").text("Altitude: "+data_array[1])
+        $("#pressure").text("Pressure: "+data_array[2])
+        $("#temperature").text("Temperature: "+data_array[3])
+        $("#gyrox").text("x: "+data_array[4])
+        $("#gyroy").text("y: "+data_array[5])
+        $("#gyroz").text("z: "+data_array[6])
+        $("#magx").text("x: "+data_array[7])
+        $("#magy").text("y: "+data_array[8])
+        $("#magz").text("z: "+data_array[9])
+        $("#rhall").text("rhall: "+data_array[10])
 
-    //$("#longitude").value = data_array
+    //$("#longitude").value = data_array // need more data for these
     //$("#latitude").value = data_array
 
     //$("#accx").value = data_array
